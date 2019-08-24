fde73b81fd3efaf84df87d6a771eb0093dc3c9e3
Yavuz Tor <yavuztor79@gmail.com>
2019-08-24 14:39:25 -0500

    Added strong name signing

---
+++ JsonLogic.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+[assembly: System.Reflection.AssemblyKeyFile("../../signingKey.snk")]
+[assembly: System.Reflection.AssemblyTitle("JsonLogic.Net.dll")]
+[assembly: System.Reflection.AssemblyDescription(".Net implementation of jsonlogic.com")]
+[assembly: System.Reflection.AssemblyVersion("1.1.7")]
+[assembly: System.Reflection.AssemblyProduct("jsonlogic.com")]
+[assembly: System.Reflection.AssemblyCompany("yavuztor@hotmail.com")]
