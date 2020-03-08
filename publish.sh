97ca41d83e84f019ab893aee796484ec00a86bf3
Yavuz Tor <yavuztor79@gmail.com>
2020-03-07 21:00:00 -0600

    Switched to using release configuration.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-rm JsonLogic.Net/bin/Debug/*.nupkg
-dotnet pack
-PKGNAME=`ls JsonLogic.Net/bin/Debug/*.nupkg`
+rm JsonLogic.Net/bin/Release/*.nupkg
+dotnet pack -c Release --no-build
+PKGNAME=`ls JsonLogic.Net/bin/Release/*.nupkg`
 echo PKGNAME is $PKGNAME
 dotnet nuget push $PKGNAME -k "$NUGET_KEY" -s "https://api.nuget.org/v3/index.json" 2>&1 || true
